0305258a8148436690f06c18dad0cb797edbfb77
Adam Hamer <hamer88@hotmail.co.uk>
2017-05-09 12:31:01 +0000

    updated index.html

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-var express = require('express');
-var app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use(express.static('public'))
+app.get('/', function(req, res){
+  res.send('index.html');
+})
+app.get('/date/:val', function(req, res, next){
+  var val = req.params.val;
+  var time = new Date(val);
+  var options = {
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+  }
+
+  if(isNaN(val)){
+    var natural = time.toLocaleDateString("en-uk", options)
+    var unix = time.getTime() / 1000;
+  }
+  else {
+    var unix = val;
+    var natural = new Date(val * 1000).toLocaleDateString("en-uk", options)
+  }
+
+  res.json({"unix": unix, "natural": natural});
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+
+app.listen(3000, function(){
+    console.log('server on 3000');
+});
